65b8840a620327b15b3a2211c27a592fcd2f2d5c
nairamatosyan <matosian.naira@gmail.com>
2023-12-17 13:31:34 +0400

    remove condition on uploaded image size

--- src/frontend/src/components/ImageUpload.js
+++ src/frontend/src/components/ImageUpload.js
@@ -15,11 +15,7 @@ const beforeUpload = (file) => {
     if (!isJpgOrPng) {
         message.error('You can only upload JPG/PNG file!');
     }
-    const isLt2M = file.size / 1024 / 1024 < 2;
-    if (!isLt2M) {
-        message.error('Image must smaller than 2MB!');
-    }
-    return isJpgOrPng && isLt2M;
+    return isJpgOrPng;
 };
 
 const ImageUpload = ({form}) => {
